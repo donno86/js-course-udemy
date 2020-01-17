af42cd79b2ac73ecf513fce7a3e8741e3745da93
Don <doncarlier@gmail.com>
2020-01-17 16:48:56 +0100

    First API setup

--- 9-Modern-JS/src/js/index.js
+++ 9-Modern-JS/src/js/index.js
@@ -1,4 +1,15 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module! Var x = ${x}`);
+import axios from 'axios';
+
+async function getResults(query) {
+  
+try {
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`)
+    const recipes = res.data.recipes;
+    console.log(recipes);
+} catch(error) {
+    alert(error);
+}
+
+}
+
+getResults('pizza');
