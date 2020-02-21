20315a3fb456abe4913210dab14bd0a0e0a02e97
Don <doncarlier@gmail.com>
2020-02-21 15:44:34 +0100

    Part 2 of nodeJS laptopshop

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -12,23 +12,38 @@ const server = http.createServer((req, res) => {
 
     if (pathname === '/products' || pathname === '/') {
         res.writeHead(200, {'Content-type': 'text/html'})
-        res.end('these should show products');
+        
+        
+
+
     } 
     
     else if (pathname === '/laptop' && id < laptopData.length) {
         res.writeHead(200, {'Content-type': 'text/html'})
-        res.end(`these should show detail laptop ${id}`);
+
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8',(err, data) => {
+            const laptop = laptopData[id];
+            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+            output = output.replace(/{%IMAGE%}/g, laptop.image);
+            output = output.replace(/{%PRICE%}/g, laptop.price);
+            output = output.replace(/{%SCREEN%}/g, laptop.screen);
+            output = output.replace(/{%CPU%}/g, laptop.cpu);
+            output = output.replace(/{%STORAGE%}/g, laptop.storage);
+            output = output.replace(/{%RAM%}/g, laptop.ram);
+            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+
+            res.end(output);
+        });
     } 
     
     else {
         res.writeHead(404, {'Content-type': 'text/html'})
         res.end('Page was not found on server!');
     }
-
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.end('this is the response');
 });
 
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests now');
 });
+
+//function replaceTemplate(originalHtml, laptop)
